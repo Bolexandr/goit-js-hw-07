b5b117b512ad4ee9eca8023db34be8eb11483eb2
Olexandr Beisiuk <57046480+Bolexandr@users.noreply.github.com>
2022-09-05 22:24:55 +0300

    Good

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,67 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const fatherDiv = document.querySelector('.gallery')
+
+
+///////////  Функція додає елементи в дом 
+function createImagesGalary() {
+
+  const htmlMarcImgs = galleryItems.reduce((longString,objImg) => {
+  const { preview, original, description } = objImg;
+  const protoString = 
+`<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  return longString += protoString;
+
+},'')
+
+  fatherDiv.insertAdjacentHTML("beforeend", htmlMarcImgs);
+
+}
+createImagesGalary()   /// виклик функції
+
+///////////////////
+
+
+function clickEvent(event) {
+  event.preventDefault()
+
+  if (event.target.localName !== 'img') {
+    return
+  }
+  const instance = basicLightbox.create(`
+   <img src=${event.target.dataset.source}>
+`)
+
+  instance.show()
+
+/////////// Видалення  по escape
+function closeByEscpt (event){
+ 
+  if (event.code === 'Escape') {
+    instance.close()
+ 
+  }
+  document.removeEventListener("keydown", closeByEscpt)
+   console.log('removeEventListener = true')
+}
+  document.addEventListener("keydown", closeByEscpt);
+  
+}
+
+
+fatherDiv.addEventListener('click', clickEvent)
+
+
+
+///////////////////////// Додаткове завдання
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,39 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const fatherDiv = document.querySelector('.gallery')
+
+///////////  Функція додає елементи в дом 
+function createImagesGalary() {
+
+  const htmlMarcImgs = galleryItems.reduce((longString,objImg) => {
+  const { preview, original, description } = objImg;
+    const protoString = 
+`<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  return longString += protoString;
+
+  }, '')
+  
+  fatherDiv.insertAdjacentHTML("beforeend", htmlMarcImgs);
+
+  const bigPhoto = new SimpleLightbox('.gallery__item', {
+
+    captionsData : 'alt',
+    captionDelay : 250,
+    
+
+  })
+  console.dir(bigPhoto)
+}
+createImagesGalary()   /// виклик функції
+
+console.dir(SimpleLightbox)
